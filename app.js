268d589435ac48b7be3a5f5d41dcf37986529249
Meeraj Kumar Sarkar <sarkarmeerajkumar@gmail.com>
2025-07-07 20:12:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -210,14 +210,9 @@ function renderFlashCard(cardData, deckName, deckId, cardId) {
       <strong>A:</strong>
       <p>${cardData.answer}</p>
     </div>
-    <button class='delete-card-btn' data-deck-id='${deckId}' data-card-id='${cardId}'>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
-    </button>
   `;
 
-    const deleteBtn = cardEl.querySelector('.delete-card-btn');
-    deleteBtn.addEventListener('click', (e) => {
-        e.stopPropagation();
+    cardEl.addEventListener('dblclick', () => {
         showConfirmationModal('Do you want to delete this flashcard?', () => {
             showLoader();
             const cardRef = ref(database, `DeckList/${deckId}/cards/${cardId}`);
